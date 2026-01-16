0dcaed788459aaa5e7b5722183b586e080f7cef6
Matt <solomatias321@gmail.com>
2026-01-16 15:28:55 +0100

    feat: set up layout

---
+++ app/(app)/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className="">
+       {/* Featured Products Carousel */}
+
+       {/* Page Banner */}
+
+       {/* Category Tiles */}
+
+      {/* New Arrivals Section */}
+    </div>
+  );
+}
