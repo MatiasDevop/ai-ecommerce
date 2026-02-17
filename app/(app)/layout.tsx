26d8ce684a81c783637a8d3e3edd5b50dc2c8407
Matt <solomatias321@gmail.com>
2026-02-17 16:55:46 +0100

    feat: add products slug and details

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,7 @@
+import { Header } from "@/components/app/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -7,9 +9,12 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <CartStoreProvider>
-        <main>{children}</main>
-        <Toaster position="bottom-center" />
-        <SanityLive />
+        <ChatStoreProvider>
+          <Header />
+          <main>{children}</main>
+          <Toaster position="bottom-center" />
+          <SanityLive />
+        </ChatStoreProvider>
       </CartStoreProvider>
     </ClerkProvider>
   );
--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -110,7 +110,7 @@ export default async function Home({ searchParams }: PageProps) {
       </div>
 
       {/* Products Section */}
-        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
         <ProductSection
           categories={categories}
           products={products}
